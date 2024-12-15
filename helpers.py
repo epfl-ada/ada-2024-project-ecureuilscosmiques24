9ff6df69451dbf1fbb592bf4d71f7ead49bcf11d
Thomas Lepère <thomas.lepere@epfl.ch>
2024-12-15 13:55:46 +0100

    prepared all the csv, started bias analysis, tree missing the scaling of box office

--- helpers.py
+++ helpers.py
@@ -69,3 +69,96 @@ def get_gender(name : str) -> str:
             gender = 'Unknown' #when there is a doubt on the gender, we replace it by unknown.
 
         return gender
+
+
+def country_to_region(country):
+    # Define lists for each region
+    north_america = [
+        "united states of america", "canada", "puerto rico", "mexico", "bahamas", "costa rica"
+    ]
+
+    south_america = [
+        "argentina", "brazil", "venezuela", "peru", "bolivia", "chile", "colombia", "cuba"
+    ]
+
+    western_europe = [
+        "united kingdom", "france", "germany", "west germany", "weimar republic",
+        "netherlands", "switzerland", "portugal", "belgium", "austria", "finland",
+        "denmark", "sweden", "norway", "iceland", "ireland", "luxembourg", "greece", "spain", "italy", "england",
+        "wales", "cyprus"
+    ]
+
+    eastern_europe = [
+        "soviet union", "poland", "hungary", "yugoslavia", "bosnia and herzegovina",
+        "czech republic", "slovakia", "slovenia", "romania", "ukraine", "bulgaria",
+        "republic of macedonia", "lithuania", "albania", "georgia", "armenia","turkey",
+        "slovak republic", "croatia", "estonia", "serbia"
+    ]
+
+    asia = [
+        "india", "japan", "hong kong", "south korea", "pakistan", "thailand", "malaysia",
+        "china", "indonesia", "taiwan", "iran", "nepal", "korea", "israel", "vietnam",
+        "sri lanka", "cambodia", "bangladesh", "uzbekistan", "bhutan", "russia", "lebanon", "united arab emirates",
+        "singapore"
+    ]
+
+    oceania = [
+        "australia", "new zealand", "isle of man", "philippines", "jamaica"
+    ]
+
+    africa = [
+        "south africa", "egypt", "nigeria", "algeria", "senegal", "afghanistan"
+    ]
+
+    # List of "dead" countries
+    dead_countries = [
+        "german democratic republic", "czechoslovakia", "kingdom of great britain",
+        "socialist federal republic of yugoslavia", "federal republic of yugoslavia",
+        "nazi germany", "mandatory palestine", "palestinian territories", "serbia and montenegro", "burma"
+    ]
+
+
+    # Merge all regional lists into one dictionary
+    country_region_map = {**{country: "North America" for country in north_america},
+                          **{country: "South America" for country in south_america},
+                          **{country: "West Europa" for country in western_europe},
+                          **{country: "East Europa" for country in eastern_europe},
+                          **{country: "Asia" for country in asia},
+                          **{country: "Oceania" for country in oceania},
+                          **{country: "Africa" for country in africa},
+                          **{country: "Dead country" for country in dead_countries}
+                          }
+
+    # We make the function insensitive to the case
+    country = country.strip().lower()
+
+    # Return the region or a default message if the country is not found
+    return country_region_map.get(country, "Unknown region")
+
+
+def min_max_scaling(data):
+    """
+    Set the maximum of the data to 1 and the minimum to 0
+    Args:
+    data: the data we want to scale
+    Returns
+    scaled_data
+    """
+    data_max = np.max(data)
+    data_min = np.min(data)
+    scaled_data = (data-data_min)/(data_max-data_min)
+    return scaled_data
+
+
+def standardize_scaling(data):
+    """
+    Set the standard deviation of the data to 1 and the mean to 0
+    Args:
+    data: the data we want to scale
+    Returns
+    scaled_data
+    """
+    data_mean = np.mean(data)
+    data_std = np.std(data)
+    scaled_data = (data-data_mean)/data_std
+    return scaled_data
