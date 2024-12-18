b45eaee6cf96abbf50b838df42175175fa4ce206
Thomas Lepère <thomas.lepere@epfl.ch>
2024-12-18 16:09:03 +0100

    premiere partie du notebook done

--- helpers.py
+++ helpers.py
@@ -2,6 +2,12 @@ from names_dataset import NameDataset
 import numpy as np
 import pandas as pd
 import ast
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics import silhouette_score
+import matplotlib.pyplot as plt
+
 
 #We create an instance of the NameDataset class
 nd = NameDataset()
@@ -69,3 +75,96 @@ def get_gender(name : str) -> str:
             gender = 'Unknown' #when there is a doubt on the gender, we replace it by unknown.
 
         return gender
+    
+
+def country_to_region(country):
+    """
+    Associate a defined region to a country
+    Input:
+    country: string
+    Output:
+    region: string
+    """
+    # Define lists for each region
+    north_america = ["united states of america", "canada", "puerto rico", "mexico", "bahamas", "costa rica"]
+    south_america = ["argentina", "brazil", "venezuela", "peru", "bolivia", "chile", "colombia", "cuba"]
+    western_europe = ["united kingdom", "france", "germany", "west germany", "weimar republic",
+                    "netherlands", "switzerland", "portugal", "belgium", "austria", "finland",
+                    "denmark", "sweden", "norway", "iceland", "ireland", "luxembourg", "greece", 
+                    "spain", "italy", "england","wales", "cyprus"]
+    eastern_europe = ["soviet union", "poland", "hungary", "yugoslavia", "bosnia and herzegovina",
+                    "czech republic", "slovakia", "slovenia", "romania", "ukraine", "bulgaria",
+                    "republic of macedonia", "lithuania", "albania", "georgia", "armenia","turkey",
+                    "slovak republic", "croatia", "estonia", "serbia"]
+    asia = ["india", "japan", "hong kong", "south korea", "pakistan", "thailand", "malaysia",
+            "china", "indonesia", "taiwan", "iran", "nepal", "korea", "israel", "vietnam",
+            "sri lanka", "cambodia", "bangladesh", "uzbekistan", "bhutan", "russia", "lebanon", 
+            "united arab emirates", "singapore"]
+    oceania = ["australia", "new zealand", "isle of man", "philippines", "jamaica"]
+    africa = ["south africa", "egypt", "nigeria", "algeria", "senegal", "afghanistan"]
+
+    # List of "dead" countries
+    dead_countries = ["german democratic republic", "czechoslovakia", "kingdom of great britain",
+                    "socialist federal republic of yugoslavia", "federal republic of yugoslavia",
+                    "nazi germany", "mandatory palestine", "palestinian territories", "serbia and montenegro", "burma"]
+
+    # Merge all regional lists into one dictionary
+    country_region_map = {**{country: "North America" for country in north_america},
+                          **{country: "South America" for country in south_america},
+                          **{country: "West Europa" for country in western_europe},
+                          **{country: "East Europa" for country in eastern_europe},
+                          **{country: "Asia" for country in asia},
+                          **{country: "Oceania" for country in oceania},
+                          **{country: "Africa" for country in africa},
+                          **{country: "Dead country" for country in dead_countries}}
+
+    # We make the function insensitive to the case
+    country = country.strip().lower()
+
+    # Return the region or a default message if the country is not found
+    return country_region_map.get(country, "Unknown region")
+
+
+def extract_numerical_categorical_features(data:pd.DataFrame, exclude_col:list):
+    """
+    Extract the categorical and numerical features of a dataset whith some excluded columns
+    Args:
+    data: the dataframe
+    label: the label we do not want to import
+    """
+    numerical_features = [key for key, value in data.dtypes.items() if value=="float64" and key not in exclude_col]
+    print(f"Numerical features: {numerical_features}")
+
+    #categorical_features = [key for key, value in data.dtypes.items() if value=="object" and key!=label]
+    categorical_features = [col for col in data.columns if col not in (numerical_features+exclude_col)]
+    print(f"Categorical features: {categorical_features}")
+    
+    return numerical_features, categorical_features
+
+
+def plot_sse(features_X, start=2, end=10):
+    """
+    Plot the sum of the squared errors
+
+    Input:
+    features
+    start: min k values
+    end: maximum k value
+
+    Output:
+    matplotlib.object
+    """
+    sse = []
+    for k in range(start, end):
+        # Assign the labels to the clusters
+        kmeans = KMeans(n_clusters=k, random_state=10).fit(features_X)
+        sse.append({"k": k, "sse": kmeans.inertia_})
+
+    sse = pd.DataFrame(sse)
+    # Plot the data
+    fig, ax = plt.subplots()
+    ax.plot(sse.k, sse.sse)
+    ax.set_xlabel("K")
+    ax.set_ylabel("Sum of Squared Errors")
+    plt.show()
+    return ax
